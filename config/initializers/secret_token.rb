2d4162a9bc92a52b4a81050c578693f542f7868e
unknown <OSSFastTrack@ossfasttrackpc.(none)>
2012-09-20 14:28:32 +0700

    init kokarat-me ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KokaratMe::Application.config.secret_token = '2f0f3ddd9bc6ee9c444af36a844fad9dba597795fe1d585bfa53886c75772ea913e3d2f1e4b9ed21a41a7d2e6def46c80ad8aee3373b1f85bb0c7c3ca802070f'
